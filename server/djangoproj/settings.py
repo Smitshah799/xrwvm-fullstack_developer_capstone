1aef7c255772c41307d00627f6eaca1003324c8d
Smitshah799 <121076559+Smitshah799@users.noreply.github.com>
2024-09-06 09:41:46 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -103,13 +103,15 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': ('django.contrib.auth.password_validation.'
-                 'UserAttributeSimilarityValidator'
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
         ),
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.'
-                 'MinimumLengthValidator'
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
         ),
     },
     {
