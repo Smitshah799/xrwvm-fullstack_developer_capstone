28b30f3bd9fda59567a4adc27c99deaa3c1d0480
Smitshah799 <121076559+Smitshah799@users.noreply.github.com>
2024-09-06 10:16:14 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,7 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv('backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://localhost:5050/")
+sentiment_analyzer_url = os.getenv(
+    'sentiment_analyzer_url',
+    default="http://localhost:5050/"
+)
+
 
 def get_request(endpoint, **kwargs):
     params = ""
@@ -25,6 +29,7 @@ def get_request(endpoint, **kwargs):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url + "analyze/" + text
     try:
@@ -34,6 +39,7 @@ def analyze_review_sentiments(text):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
+
 
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
